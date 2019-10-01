22ef43a291c74cbdfc6f8f67df3056e54143af3e
eridgers <emily.ridgers@gmail.com>
2019-10-01 18:23:15 +0100

    amend lifespan virtual, format date

--- models/author.js
+++ models/author.js
@@ -18,21 +18,14 @@ authorSchema.virtual('name').get(function() {
 
 // create lifespan virtual
 authorSchema.virtual('lifespan').get(function() {
-    return this.date_of_death.getYear() - this.date_of_birth.getYear().toString();
+    var dob = this.date_of_birth ? moment(this.date_of_birth).format('MMM-DD-YYYY') : '';
+    var dod = this.date_of_death ? moment(this.date_of_death).format('MMM-DD-YYYY') : '';
+    return dob + ' - ' + dod;
 });
 
 // create URL virtual
 authorSchema.virtual('url').get(function(){
-    return 'catalog/author/' + this._id;
-});
-
-//format date of death & birth
-authorSchema.virtual('dob').get(function(){
-    return this.date_of_birth ? moment(this.date_of_birth).format('MMM-DD-YYYY') : '';
-});
-
-authorSchema.virtual('dod').get(function(){
-    return this.date_of_death ? moment(this.date_of_death).format('MMM-DD-YYYY') : '';
+    return '/catalog/author/' + this._id;
 });
 
 module.exports = mongoose.model('Author', authorSchema);
